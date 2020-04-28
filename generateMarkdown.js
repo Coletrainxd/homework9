711515511c3b4c5e98750f441cca3a8f7961ddbb
Coletrainxd <tcgcolecarter@gmail.com>
2020-04-28 17:27:39 -0600

    final commit

---
+++ generateMarkdown.js
@@ -0,0 +1,64 @@
+function generatProjectURL(github, title) {
+  var caseTitle = title.toLowerCase().split(" ").join("-");
+  return `https://github.com/${github}/${caseTitle}`;
+}
+function renderBadge(license, github, title) {
+  if (liscense !== "none") {
+    return `[![GitHub License](http://img.shields.io/badge/license-${license}-blue.svg)](${generateProjectUrl(
+      github,
+      title
+    )})`;
+  }
+  return "";
+}
+function renderSection(license) {
+  if (license !== "none") {
+    return "This project is licensed";
+  }
+  return "";
+}
+function generateMarkdown(data) {
+  return `
+    # ${data.title}
+    ${renderLicenseBadge(data.license, data.github, data.title)}
+
+    ## Description
+    ${data.description}
+
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    ## Installation
+    To install necessary dependencies, run the following command:
+    \`\`\`
+    ${data.installation}
+    \'\'\'
+
+    ## Usage
+
+    ${data.usage}
+
+    ${renderLicenseSection(data.license)}
+    ## Contributing
+    ${data.contributing}
+
+    ## Tests
+
+    To run tests, run the following command:
+
+    \`\`\`
+    ${data.test}
+    \`\`\`
+
+    ## Questions 
+    If you have any questions about the repo, open an issue or contact [${
+      data.github
+    }](${data.url}) directly at ${data.email}.
+
+    `;
+}
+module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,19 +1,68 @@
 var fs = require("fs");
-const rl = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-rl.question("Please enter the title of the project : ", (answer1) => {
-  rl.question("Please enter a description of the project : ", (answer2) => {
-    rl.close();
+var path = require("path");
+const inquirer = require("inquirer");
+var generateMarkdown = require("./generateMarkdown");
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  {
+    type: "input",
+    name: "url",
+    message: "What is the URL for the project",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "what is the projects title",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+  },
+  {
+    type: "list",
+    name: "installation",
+    message: "What command should be run to install dependencies?",
+    default: "npm i",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+    default: "npm test",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What does the user need to know about contributing to the repo?",
+  },
+];
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+function init() {
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    writeToFile("readme.md", generateMarkdown({ ...inquirerResponses }));
   });
-});
-var answers = answer1.appendFile(answer2);
-fs.appendFile("README.md", `${answers}`, function (err) {
-  if (err) throw err;
-  console.log("Saved!");
-});
-
-function init() {}
-
+}
 init();
